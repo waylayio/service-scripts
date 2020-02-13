b574eea2fa176a887b329644be124b67ddac37ed
Anton <antondejaeger@waylay.io>
2020-02-13 10:58:49 +0100

    fix: Added suggestions from pr

--- bulk-upload-resources-python-example/upload_resources.py
+++ bulk-upload-resources-python-example/upload_resources.py
@@ -9,7 +9,7 @@ import requests
 supported_timestamps = ['%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S.%fZ']
 
 if len(sys.argv) < 2:
-    print("Usage: `python ./main.py <csv-path> <configuration-path>`")
+    print('Usage: `python ./main.py <csv-path> <configuration-path>`')
     exit()
 
 csv_file_path = sys.argv[1]
@@ -21,11 +21,11 @@ config.read(config_path)
 
 def get_auth(config: RawConfigParser):
     """ Get the authentication out of our config file """
-    return config.get("domain", "client-key"), config.get("domain", "client-secret")
+    return config.get('domain', 'client-key'), config.get('domain', 'client-secret')
 
 
 def convert_datetime_to_epoch(datetime_string: str):
-    """ Converts the datetime to epoch in miliseconds there are two possible time formats"""
+    """ Converts the datetime to epoch in milliseconds there are two possible time formats """
     try:
         return datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%SZ').timestamp() * 1000
     except:
@@ -33,7 +33,7 @@ def convert_datetime_to_epoch(datetime_string: str):
 
 
 def transform_data(data: pandas.DataFrame):
-    """Transforms data from the csv format to the expected format bij the api"""
+    """ Transforms data from the csv format to the expected format by the api """
     data['timestamp'] = data['timestamp'].apply(
         lambda x: convert_datetime_to_epoch(x))
     data['metric'] = data['metric'].apply(lambda metric: metric.replace('waylay.resourcemessage.metric.', ''))
@@ -41,8 +41,8 @@ def transform_data(data: pandas.DataFrame):
 
 
 def chunck_to_message(chunck):
-    """Makes entry in the dict with the key the value of the metric and with as value the value from de value key.
-    Also drops the unneeded entries"""
+    """ Makes entry in the dict with the key the value of the metric and with as value the value from de value key.
+    Also drops the unneeded entries """
 
     for row in chunck:
         row[row['metric']] = row.pop('value')
@@ -51,7 +51,7 @@ def chunck_to_message(chunck):
 
 
 def send_messages(data_json_array: list, config: RawConfigParser):
-    """Sends the json array to the waylay vendor in chunks of specific length (as defined in config)"""
+    """ Sends the json array to the waylay vendor in chunks of specific length (as defined in config) """
     auth = get_auth(config)
     chunck_size = int(config.get('network', 'values-per-post'))
     sent = 0
@@ -60,11 +60,11 @@ def send_messages(data_json_array: list, config: RawConfigParser):
         chunck = data_json_array[:chunck_size]
         data_json_array = data_json_array[chunck_size:]
         messages = chunck_to_message(chunck)
-        resp = requests.post("%s/messages?store=true&forward=false" % (config.get("domain", "data-path")),
+        resp = requests.post('%s/messages?store=true&forward=false' % (config.get('domain', 'data-path')),
                              json=messages, auth=auth)
 
         if not resp.ok:
-            raise ConnectionError("Uploading failed.", resp.json())
+            raise ConnectionError('Uploading failed.', resp.json())
         else:
             sent += chunck_size
             print('Sent ' + str(sent) + ' of ' + str(total))
@@ -74,23 +74,24 @@ def create_resource(resource_name: str, config: RawConfigParser):
     """ Create a resource """
 
     auth = get_auth(config)
-    resp = requests.post(config.get("domain", "resource-path") + "/api/resources/", auth=auth, json={
-        "id": resource_name,
-        "name": resource_name
+    resp = requests.post(config.get('domain', 'resource-path') + '/api/resources/', auth=auth, json={
+        'id': resource_name,
+        'name': resource_name
     })
 
     if not resp.ok:
-        raise ConnectionError("Creating resource failed.", resp.json())
+        raise ConnectionError('Creating resource failed.', resp.json())
 
 
 def send_bulk_data(data: pandas.DataFrame, config: RawConfigParser):
-    """Transforms data to json array and sends it to waylay service"""
+    """ Transforms data to json array and sends it to waylay service """
     data_json_str = data.to_json(orient='records')
     data_json_array = json.loads(data_json_str)
     send_messages(data_json_array, config)
 
 
 def create_resources(data: pandas.DataFrame, config: RawConfigParser):
+    """ Extracts resource names from the data and sends it to waylay data service  """
     resource_name_list = data['resource'].unique()
     for resource_name in resource_name_list:
         try:
